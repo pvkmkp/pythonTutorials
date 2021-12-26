a6f7e61723abcddb8e9d37535e26fc3213c860a0
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2021-12-27 02:05:47 +0530

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,53 @@
+print("*** START : INTEGER ***")
+myInt=10
+print(myInt)
+print(type(myInt))
+print("*** END : INTEGER ***")
+
+print("*** START : FLOAT ***")
+myFloat=10.5
+print(myFloat)
+print(type(myFloat))
+print("*** END : FLOAT ***")
+
+print("*** START : STRING ***")
+myString="vijaykumar pinninti"
+print(myString)
+print(type(myString))
+print("*** END : STRING ***")
+
+print("*** START : LIST ***")
+myPlayList=["Ilaya Raja","Deva","A.R. Rahman","Mani sharma","Harris jayaraj"]
+print(myPlayList)
+print(type(myPlayList))
+print("*** END : LIST ***")
+
+print("*** START : DICTIONARY ***")
+myDictionary={
+    "Ilaya Raja":20,
+    "Deva":20,
+    "A.R. Rahman":20,
+    "Mani sharma":20,
+    "Harris jayaraj":20
+    }
+print(myDictionary)
+print(type(myDictionary))
+print("*** END : DICTIONARY ***")
+
+print("*** START : TUPLE ***")
+myPlayListTuple=("Ilaya Raja","Deva","A.R. Rahman","Mani sharma","Harris jayaraj")
+print(myPlayListTuple)
+print(type(myPlayListTuple))
+print("*** END : TUPLE ***")
+
+print("*** START : SET ***")
+myPlayListSet={"Ilaya Raja","Deva","A.R. Rahman","Mani sharma","Harris jayaraj"}
+print(myPlayListSet)
+print(type(myPlayListSet))
+print("*** END : SET ***")
+
+print("*** START : BOOLEAN ***")
+isTrue=10>9
+print(isTrue)
+print(type(isTrue))
+print("*** END : BOOLEAN ***")
