3afb6cd822b55b2a91445c459e252a402e97c94c
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2021-12-30 20:13:45 +0530

    Add files via upload

---
+++ AssignmentOperators.py
@@ -0,0 +1,104 @@
+# print("***startAnd***")
+# a=10
+# b=20
+# c=a+b
+# print("c=",c)
+# print("***endAnd***")
+
+# print("***startAddAnd***")
+# a=10
+# c=0
+# c=c+a
+# print("c=",c)
+# print("***endAddAnd***")
+
+# print("***startAddAnd***")
+# a=10
+# c=0
+# c+=a
+# print("c=",c)
+# print("***endAddAnd***")
+
+# print("***startSubtractAnd***")
+# a=10
+# c=20
+# c=c-a
+# print("c=",c)
+# print("***endSubtractAnd***")
+
+# print("***startSubtractAnd***")
+# a=10
+# c=20
+# c-=a
+# print("c=",c)
+# print("***endSubtractAnd***")
+
+# print("***startMultiplyAnd***")
+# a=20
+# c=10
+# c=c*a
+# print("c=",c)
+# print("***endMultiplyAnd***")
+
+# print("***startMultiplyAnd***")
+# a=20
+# c=10
+# c*=a
+# print("c=",c)
+# print("***endMultiplyAnd***")
+
+# print("***startDevideAnd***")
+# a=10
+# c=20
+# c=c/a
+# print("c=",c)
+# print("***endDevideAnd***")
+
+# print("***startDevideAnd***")
+# a=10
+# c=20
+# c/=a
+# print("c=",c)
+# print("***endDevideAnd***")
+
+# print("***startModulusAnd***")
+# a=10
+# c=20
+# c=c%a
+# print("c=",c)
+# print("***endModulusAnd***")
+
+# print("***startModulusAnd***")
+# a=10
+# c=20
+# c%=a
+# print("c=",c)
+# print("***endModulusAnd***")
+
+# print("***startFloorDevideAnd***")
+# a=10
+# c=20
+# c=c//a
+# print("c=",c)
+# print("***endFloorDevideAnd***")
+
+# print("***startFloorDevideAnd***")
+# a=10
+# c=20
+# c//=a
+# print("c=",c)
+# print("***endFloorDevideAnd***")
+
+# print("***startExponentAnd***")
+# a=2
+# c=10
+# c=c**a
+# print("c=",c)
+# print("***endExponentAnd***")
+
+# print("***startExponentAnd***")
+# a=2
+# c=10
+# c**=a
+# print("c=",c)
+# print("***endExponentAnd***")
