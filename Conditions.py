ed614b0faacb771733f8db7841db948c7d648534
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2022-01-02 19:36:16 +0530

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,61 @@
+# print("=============startIf================")
+# x=10
+# if(x>5):
+#     print("x is greater than 5")
+
+# x=10
+# if(x>5):print("x is greater than 5")
+
+# x=10
+# if(x>50):
+#     print("x is greater than 50")
+
+# x=10
+# if(x>50):print("x is greater than 50")
+# print("===============endIf================")
+
+# print("=============startIfElse================")
+# x=10
+# if(x>5):
+#     print("x is greater than 5")
+# else:
+#     print("x is not greater than 5")
+
+# x=10
+# print("x is greater than 5") if(x>5) else print("x is not greater than 5")
+
+# x=10
+# if(x>50):
+#     print("x is greater than 50")
+# else:
+#     print("x is not greater than 50")
+
+# x=10
+# print("x is greater than 50") if(x>50) else print("x is not greater than 50")
+# print("=============endIfElse================")
+
+# print("=============startIfElifElse================")
+# x=10
+# if(x>2):
+#     print("statement 1 executed")
+# elif(x>6):
+#     print("statement 2 executed")
+# else:
+#     print("Above statements are false")
+
+# x=10
+# if(x>20):
+#     print("statement 1 executed")
+# elif(x>6):
+#     print("statement 2 executed")
+# else:
+#     print("Above statements are false")
+
+# x=10
+# if(x>20):
+#     print("statement 1 executed")
+# elif(x>30):
+#     print("statement 2 executed")
+# else:
+#     print("Above statements are false")
+# print("=============endIfElifElse================")
