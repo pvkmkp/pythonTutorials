3c55ca1f094549382bcd00a4a52f164878820a20
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2022-01-10 21:34:20 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,99 @@
+# myPlayLists=["IlayaRaja","Deva","AR Rahman","Manisharma"]
+# for myPlayLists in myPlayLists:
+#     print(myPlayLists)
+
+# myPairList=[(0,1),(1,2),(2,3),(3,4)]
+# for myPairList in myPairList:
+#     print(myPairList)
+
+# myPairList=[(0,1),(1,2),(2,3),(3,4)]
+# for a,b in myPairList:
+#     print(a)
+
+# myPairList=[(0,1),(1,2),(2,3),(3,4)]
+# for a,b in myPairList:
+#     print(b)
+
+# myPairList=[(0,1,2),(1,2,3),(2,3,4),(3,4,5)]
+# for a,b,c in myPairList:
+#     print(c)
+
+# myPlayLists={"IlayaRaja","Deva","AR Rahman","Manisharma"}
+# for myPlayLists in myPlayLists:
+#     print(myPlayLists)
+
+# myPairList={(0,1,2),(1,2,3),(2,3,4),(3,4,5)}
+# for myPairList in myPairList:
+#     print(myPairList)
+
+# myPairList={(0,1,2),(1,2,3),(2,3,4),(3,4,5)}
+# for a,b,c in myPairList:
+#     print(a)
+
+# myPairList={(0,1,2),(1,2,3),(2,3,4),(3,4,5)}
+# for a,b,c in myPairList:
+#     print(b)
+
+# myPairList={(0,1,2),(1,2,3),(2,3,4),(3,4,5)}
+# for a,b,c in myPairList:
+#     print(c)
+
+# myPlayLists=("IlayaRaja","Deva","AR Rahman","Manisharma")
+# for myPlayLists in myPlayLists:
+#     print(myPlayLists)
+
+# myPairList=((0,1,2),(1,2,3),(2,3,4),(3,4,5))
+# for myPairList in myPairList:
+#     print(myPairList)
+
+# myPairList=((0,1,2),(1,2,3),(2,3,4),(3,4,5))
+# for a,b,c in myPairList:
+#     print(a)
+
+# myPairList=((0,1,2),(1,2,3),(2,3,4),(3,4,5))
+# for a,b,c in myPairList:
+#     print(b)
+
+# myPairList=((0,1,2),(1,2,3),(2,3,4),(3,4,5))
+# for a,b,c in myPairList:
+#     print(c)
+
+# myPlayListDict={
+#     "IlayaRaja":20,
+#     "Deva":20,
+#     "AR Rahman":20,
+#     "Manisharma":20
+# }
+# for myPlayListdict in myPlayListDict:
+#     print(myPlayListdict)
+
+# myPlayListDict={
+#     "IlayaRaja":20,
+#     "Deva":20,
+#     "AR Rahman":20,
+#     "Manisharma":20
+# }
+# for myPlayListdict in myPlayListDict:
+#     print(myPlayListDict[myPlayListdict])
+
+# myPlayListDict={
+#     "IlayaRaja":20,
+#     "Deva":20,
+#     "AR Rahman":20,
+#     "Manisharma":20
+# }
+# for myPlayListdict in myPlayListDict.values():
+#     print(myPlayListdict)
+
+# myPlayListDict={
+#     "IlayaRaja":20,
+#     "Deva":20,
+#     "AR Rahman":20,
+#     "Manisharma":20
+# }
+# for first, second in myPlayListDict.items():
+#     print(first,second)
+
+# myName="Vijaykumar"
+# for myName in myName:
+#     print(myName)
