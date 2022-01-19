aa67e1e58bc527d853fa2f796792ed834476b3c6
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2022-01-19 12:02:48 +0530

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,62 @@
+""" print("=============== start ===================")
+i=0
+while(i<5):
+    print("i am in while",i)
+    i=i+1
+print("================ end ====================") """
+
+""" print("=============== start ===================")
+i=0
+while(i<5):print("i am in while",i); i=i+1
+print("================ end ====================") """
+
+""" i=0
+inputString="vijaykumar"
+print(len(inputString))
+while(i<len(inputString)):
+    print("I am Statement 1")
+    if(inputString[i]=="j"):
+        print("I am statement 2")
+        i=i+1
+        print("I am statement 3")
+        continue
+
+    print("I am statement 4")
+    print("================")
+    i=i+1 """
+
+""" i=0
+inputString="vijaykumar"
+print(len(inputString))
+while(i<len(inputString)):
+    print("I am Statement 1")
+    if(inputString[i]=="j"):
+        print("I am statement 2")
+        i=i+1
+        print("I am statement 3")
+        break
+    
+    print("I am statement 4")
+    print("================")
+    i=i+1 """
+
+""" i=0
+inputString="vijaykumar"
+print(len(inputString))
+while(i<len(inputString)):
+    print("I am Statement 1")
+    if(inputString[i]=="n"):
+        pass
+    
+    print("I am statement 4")
+    print("================")
+    i=i+1 """
+
+""" i=0
+while(i<5):
+    print("Hi Vijaykumar nice to meet you")
+    i=i+1
+
+else:
+    print("Bye Vijaykumar will see you again")
+print("=====================================") """
