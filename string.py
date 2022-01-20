8e3a794db56b00c2917c11d26bdff3f83bf5c035
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2022-01-21 00:59:06 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,30 @@
+# myString="I am string"
+# print(type(myString))
+# print(myString)
+# print("==================================")
+
+# Multi line string
+
+# myString="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+# Nullam vehicula ipsum a arcu cursus.
+# Porta non pulvinar neque laoreet suspendisse interdum consectetur."""
+# print(myString)
+# print(type(myString))
+# print("===================================")
+
+# string concatenation
+
+# stringConcat1="Vijay"
+# stringConcat2="kumar"
+# stringConcat3="pinninti"
+# print(stringConcat1+stringConcat2+" "+stringConcat3)
+# print("=====================================")
+
+# stringConcat1="10"
+# stringConcat2="20"
+# print(stringConcat1+stringConcat2)
+# print("=====================================")
+
+# iamSleeping="z"
+# print(iamSleeping*20)
+# print("======================================")
