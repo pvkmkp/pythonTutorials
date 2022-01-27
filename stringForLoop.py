650d077dfd81012bd781f66a72bc3ae1e82a21a7
pvkmkp <96689142+pvkmkp@users.noreply.github.com>
2022-01-27 17:37:01 +0530

    Add files via upload

---
+++ stringForLoop.py
@@ -0,0 +1,35 @@
+# myString="vijaykumar"
+# for mystring in myString:
+#     print(mystring)
+
+# myString="1234"
+# for mystring in myString:
+#     print(mystring)
+
+# in
+
+# myString="vijaykumar"
+# print("vijay" in myString)
+
+# myString="vijaykumar"
+# print("t" in myString)
+
+# if
+
+# myString="vijaykumar"
+# if "vijay" in myString:
+#     print("vijay exist in vijaykumar")
+
+# myString="vijaykumar"
+# if "t" in myString:
+#     print("t is exist")
+# else:
+#     print("t is not exist")
+
+# not
+
+# myString="vijaykumar"
+# if "t" not in myString:
+#     print("t is not exist")
+# else:
+#     print("t is exist")
---
+++ stringFormatting.py
@@ -0,0 +1,33 @@
+# print("%(name)s is %(behaviour)s boy"%{"name":"vijaykumar","behaviour":"good"})
+
+# name="vijaykumar"
+# behaviour="good"
+# print(f"{name} is {behaviour} boy")
+
+# behaviour="good"
+# myString="vijaykumar is {} boy"
+# print(myString.format(behaviour))
+
+# videos=143
+# myString="The channel contains {:.2f} hours of videos"
+# print(myString.format(videos))
+
+# myString="{name} is an {intro} Its design philosophy emphasizes code {cr} with its use of significant indentation."
+# print(myString.format(name="Python", intro="interpreted high-level general-purpose programming language.", cr="readability"))
+
+# name="Python"
+# intro="interpreted high-level general-purpose programming language."
+# cr="readability"
+# myString="{} is an {} Its design philosophy emphasizes code {} with its use of significant indentation."
+# print(myString.format(name,intro,cr))
+
+# name="Python"
+# intro="interpreted high-level general-purpose programming language."
+# cr="readability"
+# myString="{0} is an {1} Its design philosophy emphasizes code {2} with its use of significant indentation."
+# print(myString.format(name,intro,cr))
+
+# piValue=22/7
+# print(piValue)
+# print("Value of pi is {pi}".format(pi=piValue))
+# print("Value of pi is {pi:17.3f}".format(pi=piValue))
